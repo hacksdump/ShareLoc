eae2dcbcabd99eef2424610eab75d81808c38574
Harshit Maurya <hmaurya999@gmail.com>
2019-04-16 07:24:37 +0530

    Main entry point

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "shareloc.h"
+#include "backgroundbroadcastsender.h"
+#include <QtConcurrent>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    BackgroundBroadcastSender sender;
     ShareLoc w;
     w.show();
 
